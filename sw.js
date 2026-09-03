b17e2fe4c471cda9137bfae2b173cd5a87a1c83f
Raflyy22 <99198230+Raflyy22@users.noreply.github.com>
2026-09-03 15:19:59 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'cloudvault-v10';
+const CACHE = 'cloudvault-v11';
 const CORE = ['/', '/index.html', '/database.json', '/manifest.json'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
